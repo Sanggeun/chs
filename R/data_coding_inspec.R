9288f180658e0cb520b1a89bd2232ad3292dc9a3
baesg <baesg.pm@gmail.com>
2019-08-26 16:13:22 +0900

    il_a0800

--- R/data_coding_inspec.R
+++ R/data_coding_inspec.R
@@ -1053,15 +1053,33 @@ data_coding_inspec <- function(data_set, year) {
   data_set$il_a0702 <- NA
   data_set$il_a0703 <- NA
 
-  data_set$il_a0701 <- ifelse(data_set$il_a0200 == 1 & data_set$hya_11a1 ==1, 1,
-                              ifelse(data_set$il_a0200 == 1 & data_set$hya_11a1 == 2, 0, NA))
   if(year %in% 2011:2017) {
+    data_set$il_a0701 <- ifelse(data_set$il_a0200 == 1 & data_set$hya_11a1 == 1, 1,
+                                ifelse(data_set$il_a0200 == 1 & data_set$hya_11a1 == 2, 0, NA))
     data_set$il_a0702 <- ifelse(data_set$il_a0200 == 1 & data_set$hya_11b1 ==1, 1,
                                 ifelse(data_set$il_a0200 == 1 & data_set$hya_11b1 == 2, 0, NA))
 
+    data_set$il_a0703 <- ifelse(data_set$il_a0200 == 1 & data_set$hya_11c1 ==1, 1,
+                                ifelse(data_set$il_a0200 == 1 & data_set$hya_11c1 == 2, 0, NA))
+
   }
-  data_set$il_a0703 <- ifelse(data_set$il_a0200 == 1 & data_set$hya_11c1 ==1, 1,
-                              ifelse(data_set$il_a0200 == 1 & data_set$hya_11c1 == 2, 0, NA))
+
+  if (year == 2018) {
+    data_set$il_a0701 <- ifelse(data_set$il_a0200 == 1 & data_set$hya_11a1 == 1, 1,
+                                ifelse(data_set$il_a0200 == 1 & data_set$hya_11a1 == 2, 0, NA))
+
+    data_set$il_a0703 <- ifelse(data_set$il_a0200 == 1 & data_set$hya_11c1 ==1, 1,
+                                ifelse(data_set$il_a0200 == 1 & data_set$hya_11c1 == 2, 0, NA))
+  }
+
+  if (year == 2019) {
+    data_set$il_a0701 <- ifelse(data_set$il_a0200 == 1 & data_set$hya_11a2 == 1, 1,
+                                ifelse(data_set$il_a0200 == 1 & data_set$hya_11a2 == 2, 0, NA))
+
+    data_set$il_a0703 <- ifelse(data_set$il_a0200 == 1 & data_set$hya_11c2 ==1, 1,
+                                ifelse(data_set$il_a0200 == 1 & data_set$hya_11c2 == 2, 0, NA))
+  }
+
 
   ### 고혈압 관리교육 이수율
   if (year %in% 2011:2017) {
@@ -1073,11 +1091,15 @@ data_coding_inspec <- function(data_set, year) {
                                   data_set$il_a0703 == 0, 0, NA))
 
   }
-  if (year %in% 2018:2019) {
+  if (year ==2018) {
   data_set$il_a0800 <- ifelse(data_set$il_a0701 == 1 | data_set$il_a0703 == 1, 1,
                               ifelse(data_set$il_a0701 == 0 & data_set$il_a0703 == 0, 0, NA))
   }
 
+  if (year == 2019) {
+    data_set$il_a0800 <- ifelse(data_set$il_a0701 == 1 | data_set$il_a0703 == 1, 1,
+                                ifelse(data_set$il_a0701 == 0 & data_set$il_a0703 == 0, 0, NA))
+  }
 
   #### 당뇨병 의사진단경험률(30세이상)
   #분모: age>= 30
