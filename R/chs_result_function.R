bdb92ebed2b71203bf5c892b70aec654bf9eaa79
baesg <baesg.pm@gmail.com>
2019-08-31 17:12:59 +0900

    10

--- R/chs_result_function.R
+++ R/chs_result_function.R
@@ -90,6 +90,86 @@ chs_c_rate <- function(data, by_var = "sex" , de_var = "sm_a0100", cluster = "JI
 }
 
 
+
+#' Calculating crude mean.
+#'
+#' chs_c_meane function will calculate crude mean.
+#'
+#' @param data data set.
+#' @param by_var strata variable.
+#' @param de_var target(dependent) variable.
+#' @param cluster cluster.
+#' @param strata strate.
+#' @param index_subset denominator condition by health indicators.
+#' @param josa_year the year of data.
+#' @param index_year the years in which the target variable was investigated.
+#' @param weight weight.
+#' @param digits integer indicating the number of decimal places.#'
+#' @return crude rate of health indicators
+#' @examples
+#' @export
+#' @importFrom dplyr filter
+#' @importFrom survey svydesign
+#' @importFrom survey svymean
+
+chs_c_mean <- function(data, by_var = "sex" , de_var = "sm_a0100", cluster = "JIJUM_CD",
+                       strata=c("BOGUN_CD","dong_type","house_type"), index_subset = NULL,
+                       josa_year , index_year = 2011:2018, var_cat = 1,
+                       weight="wt",digits=1) {
+
+  # 지표별 조사연도(조사되지 않은 연도가 있음)
+  if (!all(index_year %in% josa_year)) {
+    data <- data[data$josa_year %in% josa_year,]
+  }
+
+  # 지표별 분모의 조건
+  if (!is.null(index_subset)) {
+    if(length(index_subset) == 1) {
+      data <- data %>%
+        filter(data[[index_subset]]==1)
+    } else if (length(index_subset == 2)) {
+      data <- data %>%
+        filter(data[[index_subset[1]]]==1, data[[index_subset[2]]]==1)
+    }
+  }
+
+  data$target <- data[[de_var]]
+  chs_design_1 <- svydesign(id=as.formula(paste0("~",cluster)),
+                            strata = as.formula(paste0("~",paste(strata, collapse = "+"))),
+                            weights = as.formula(paste0("~",weight)), nest = TRUE ,data=data)
+  options(survey.lonely.psu = "adjust")
+
+  #n < 50 : Don't analyze(low reliability)
+
+  if (NROW(data) >= 50) {
+
+    if (var_cat == 1) {
+
+      a <- svymean(target, chs_design_1)
+
+      result <- c(
+        by_var = data[[by_var]][1],
+        a
+
+      )
+    } else if (var_cat == 2) {
+      a <- svymean(target, chs_design_1)
+
+      result <- c(
+        by_var = data[[by_var]][1],
+        a
+      )
+    }
+
+  } else {
+    result <- c(by_var = data[[by_var]][1],
+    NA, NA)
+
+  }
+  return(result)
+}
+
+
 #' Calculating crude rate by the stratification variable.
 #'
 #' chs_c_rate_by function will calculate crude rate of health indicators by the stratification variable.
